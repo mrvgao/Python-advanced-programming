b9758f7ed26d549402953c6db0fbe3961698d539
fortymiles <minchiuan@zju.edu.cn>
2022-03-27 14:32:22 +0800

    add generator

---
+++ lesson-02/generator_show.py
@@ -0,0 +1,46 @@
+import time
+from collections import defaultdict
+import datetime
+
+
+def count_words(filename):
+    counts = defaultdict(int)
+    
+    time.sleep(1)
+
+    return counts
+
+def get_all_results(files):
+    results = []
+
+    for f in files:
+        yield count_words(f)
+
+    
+
+def update_remote_db(k, v):
+    pass
+
+def collect_results(files):
+    results = defaultdict(int)
+
+    for c in get_all_results(files):
+        print('get one {}'.format(datetime.datetime.now()))
+        for k, v in c:
+            update_remote_db(k, v)
+            results[k] += v
+
+
+if __name__  == '__main__':
+    files = ['some_file'] * 10000
+    # 9999 Error
+    print('programming running at {}'.format(datetime.datetime.now()))
+    collect_results(files)
+
+    
+    
+
+    
+
+
+
